deb9088ae46c7e97524b4aeeb207f315c277c7b3
Philipp Korber <philipp@korber.dev>
2021-01-29 13:00:14 +0100

    Added the various try_from_ implementations.

--- src/smallvec_v1.rs
+++ src/smallvec_v1.rs
@@ -1,16 +1,36 @@
 use std::{fmt::{self, Debug}, ops::Deref, cmp::{Ord, Ordering, PartialEq, Eq}, hash::{Hash, Hasher}};
+use super::Size0Error;
 
 use smallvec_v1_ as smallvec;
 use smallvec::*;
 
+pub use crate::__smallvec1_macro_v1 as smallvec1;
+
+type Result<T> = std::result::Result<T, Size0Error>;
 
 /// A macro similar to `vec!` to create a `SmallVec1`.
 ///
 /// If it is called with less then 1 element a
 /// compiler error is triggered (using `compile_error`
 /// to make sure you know what went wrong).
+///
+/// Import this from `vec1::smallvec_v1::smallvec1`. But
+/// due to limitations of of rusts macro system and
+/// the fact that there will be a separate support for
+/// SmallVec v2 without making a braking change we had
+/// to name it `__smallvec1_macro_v1` and then reexport
+/// `vec1::__smallvec1_v1` in `vec1::smallvec_v1` as
+/// `smallvec1`.
+///
+/// # Example
+///
+/// ```rust
+/// use vec1::smallvec_v1::{smallvec1, SmallVec1};
+/// let v: SmallVec1<[u8; 4]> = smallvec1![1u8, 2];
+//  assert_eq!(v, vec![1,2]);
+/// ```
 #[macro_export]
-macro_rules! smallvec1 {
+macro_rules! __smallvec1_macro_v1 {
     () => (
         compile_error!("SmallVec1 needs at least 1 element")
     );
@@ -19,7 +39,7 @@ macro_rules! smallvec1 {
     );
     ($first:expr $(, $item:expr)* ) => ({
         let smallvec = $crate::smallvec_v1_::smallvec!($first $(, $item)*);
-        SmallVec1(smallvec)
+        SmallVec1::try_from_smallvec(smallvec).unwrap()
     });
 }
 
@@ -71,6 +91,69 @@ where
         let mut vec = SmallVec::with_capacity(capacity);
         vec.push(first);
         SmallVec1(vec)
+    }
+
+    /// Tries to create a `SmallVec1<[T; _]>` from a normal `Vec<T>`.
+    ///
+    /// The size of the buffer is inferred, which means you will likely
+    /// need type annotations when calling this method;
+    ///
+    /// # Errors
+    ///
+    /// This will fail if the input `Vec<T>` is empty.
+    /// The returned error is a `Size0Error` instance, as
+    /// such this means the _input vector will be dropped if
+    /// it's empty_. But this is normally fine as it only
+    /// happens if the `Vec<T>` is empty.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// # use vec1::smallvec_v1::{smallvec1, SmallVec1};
+    /// let sv1 = SmallVec1::<[u8; 4]>::try_from_vec(vec![3, 2, 4]);
+    /// let b: SmallVec1<[u8; 4]> = smallvec1![3u8, 2, 4];
+    /// assert_eq!(sv1, Ok(b));
+    /// ```
+    pub fn try_from_vec(vec: Vec<A::Item>) -> Result<Self> {
+        if vec.is_empty() {
+            Err(Size0Error)
+        } else {
+            Ok(SmallVec1(SmallVec::from_vec(vec)))
+        }
+    }
+
+    /// Tries to create a `SmallVec1<A>` from a normal `SmallVec<A>`.
+    ///
+    /// # Errors
+    ///
+    /// This will fail if the input `Vec<T>` is empty.
+    /// The returned error is a `Size0Error` instance, as
+    /// such this means the _input vector will be dropped if
+    /// it's empty_. But this is normally fine as it only
+    /// happens if the `Vec<T>` is empty.
+    ///
+    pub fn try_from_smallvec(smallvec: SmallVec<A>) -> Result<Self> {
+        if smallvec.is_empty() {
+            Err(Size0Error)
+        } else {
+            Ok(SmallVec1(smallvec))
+        }
+    }
+
+    /// See [`SmallVec::from_buf()`] but fails if the `buf` is empty.
+    pub fn try_from_buf(buf: A) -> Result<Self> {
+        Self::try_from_smallvec(SmallVec::from_buf(buf))
+    }
+
+    /// See [`SmallVec::from_buf_and_len()`] but fails if the buf and len are empty.
+    ///
+    /// # Panic
+    ///
+    /// Like [`SmallVec::from_buf_and_len()`] this fails if the length is > the
+    /// size of the buffer. I.e. `SmallVec1::try_from_buf_and_len([] as [u8;0],2)` will
+    /// panic.
+    pub fn try_from_buf_and_len(buf: A, len: usize) -> Result<Self> {
+        Self::try_from_smallvec(SmallVec::from_buf_and_len(buf, len))
     }
 
 }
@@ -214,6 +297,10 @@ mod tests {
         let a: SmallVec1<[String; 4]>  = smallvec1!["hy".to_owned()];
         let b: SmallVec1<[&'static str; 4]>  = smallvec1!["hy"];
         assert_eq!(a,b);
+
+        let a: SmallVec1<[u8; 4]> = smallvec1![1,2,3,4,5];
+        let b: SmallVec1<[u8; 8]> = smallvec1![1,2,3,4,5];
+        assert_eq!(a,b);
     }
 
     #[test]
@@ -222,8 +309,6 @@ mod tests {
         let b: SmallVec1<[u8; 4]> = smallvec1![1, 3];
         assert_eq!(Ord::cmp(&a,&b), Ordering::Less);
     }
-
-
 
     #[test]
     fn impl_hash() {
@@ -270,4 +355,48 @@ mod tests {
         assert_eq!(a.is_empty(), false);
         assert_eq!(a.capacity(), 4/*yes 4!*/);
     }
+
+    #[test]
+    fn try_from_vec() {
+        let a = SmallVec1::<[u8; 4]>::try_from_vec(vec![1,2,3]);
+        assert_eq!(a, Ok(smallvec1![1,2,3]));
+
+        let b = SmallVec1::<[u8; 4]>::try_from_vec(vec![]);
+        assert_eq!(b, Err(Size0Error));
+    }
+
+    #[test]
+    fn try_from_smallvec() {
+        let a = SmallVec1::<[u8; 4]>::try_from_smallvec(smallvec![32,2,3]);
+        assert_eq!(a, Ok(smallvec1![32,2,3]));
+
+        let a = SmallVec1::<[u8; 4]>::try_from_smallvec(smallvec![]);
+        assert_eq!(a, Err(Size0Error));
+    }
+
+    #[test]
+    fn try_from_buf() {
+        let a = SmallVec1::try_from_buf([1u8, 2, 3, 4]);
+        assert_eq!(a, Ok(smallvec1![1,2,3,4]));
+
+        let a = SmallVec1::try_from_buf([] as [u8; 0]);
+        assert_eq!(a, Err(Size0Error));
+    }
+
+    #[test]
+    fn try_from_buf_and_len() {
+        let a = SmallVec1::try_from_buf_and_len([1u8, 2, 3, 4, 0, 0, 0, 0], 4);
+        assert_eq!(a, Ok(smallvec1![1,2,3,4]));
+
+        let a = SmallVec1::try_from_buf_and_len([1u8, 2, 3], 0);
+        assert_eq!(a, Err(Size0Error));
+    }
+
+    #[should_panic]
+    #[test]
+    fn try_from_buf_and_len_panic_if_len_gt_size() {
+        let _ = SmallVec1::try_from_buf_and_len([] as [u8; 0], 3);
+    }
+
+
 }
